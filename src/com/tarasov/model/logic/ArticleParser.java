54ec0198bf0b864094ca25e43c8e5a43fe93296e
AntonHome <cubsplanet@gmail.com>
2017-01-02 15:15:37 +0200

    page one (ex  : 1-25) : page separated by split(String regex) in 26 parts

--- src/com/tarasov/model/logic/ArticleParser.java
+++ src/com/tarasov/model/logic/ArticleParser.java
@@ -4,7 +4,6 @@ import com.tarasov.model.entity.Article;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -18,9 +17,9 @@ public class ArticleParser {
     public static void main(String[] args) throws IOException {
 
 //        articleParser();
-//        textParser();
-
-
+          textParser();
+//        test();
+//            iterateOverElements();
     }
 
     public static void articleParser() throws IOException {
@@ -44,26 +43,56 @@ public class ArticleParser {
     public static void textParser() throws IOException {
 
         List<Article> al = new ArrayList<>();
-        Document dc = Jsoup.connect("http://njnj.ru/golits/golits1-25_article.htm").get();
-        dc.select("span.c2").forEach(Element::remove);
+        Document dc = Jsoup.connect("http://njnj.ru/golits/golits1-25_article.htm").get();   // 1-25
+//        Document dc = Jsoup.connect("http://njnj.ru/golits/golits26-40_zero_article_exercise.htm").get();
+//        dc.select("span.c2").forEach(Element::remove);
+        dc.select("span.c2").remove();
+        dc.select("br").remove();
+//        dc.select("br").forEach(Element::remove);
         Element body = dc.body();
         String html = body.toString();
-
 //        System.out.println(html);
-        Pattern pat = Pattern.compile("(?i)</h2>(.+?)<h2");
+        Pattern pat = Pattern.compile("<h2 (.+?) (?=</td>)");
         Matcher matcher = pat.matcher(html);
         int start = 0;
         int count = 0;
+        String value="";
         while (matcher.find(start)) {
-            String value = html.substring(matcher.start(), matcher.end());
-            count++;
-            System.out.println(count + " - " + value);
+            value = html.substring(matcher.start(), matcher.end());
+//            count++;
+//            System.out.println(count + " - " + value);
             start = matcher.end();
         }
+        String [] valuesArray = value.split("<h2(.+?)</h2>");
+        for (String st : valuesArray){
+            count++;
+            System.out.println( count + "\t-\t" + st);
+        }
+//        System.out.println("the amount of the lines  is " + count);
+    }
 
-        System.out.println( "the amount of the lines  is " + count);
+    public static void test() throws IOException {
+
+        List<Article> al = new ArrayList<>();
+        Document dc = Jsoup.connect("http://njnj.ru/golits/golits1-25_article.htm").get();
+        Elements tdTagElements = dc.getElementsByTag("td");
+//        for (Element el : tdTagElements) {
+//            System.out.println(el.ownText());
+//            System.out.println("-------");
+//        }
+        System.out.println(tdTagElements.get(1).ownText());
+        String [] st = tdTagElements.get(1).ownText().split("...");
+
     }
 
 
+    public static void iterateOverElements () throws IOException{
 
+        Document dc = Jsoup.connect("http://njnj.ru/golits/golits26-40_zero_article_exercise.htm").get();
+        Elements elements = dc.getAllElements();
+        for (Element element : elements){
+
+        }
+
+    }
 }
